64a214ef34b8ab5d81996871a837fa8da8b50926
Harsh Sharma <harsh.sharma.001@juspay.in>
2025-06-27 19:18:33 +0530

    hold to delete

--- app/craft/hold-to-delete/page.tsx
+++ app/craft/hold-to-delete/page.tsx
@@ -1,15 +1,32 @@
 "use client";
 import CraftHeader from "@/components/ui/CraftHeader";
-import React from "react";
+import React, { useState } from "react";
 import "./hold-to-delete.css";
+import Image from "next/image";
 
 const page = () => {
+  const [slowedDown, setSlowedDown] = useState(false);
   return (
     <div className="p-4 pt-20 max-w-3xl mx-auto bg-[var(--background)] min-h-screen">
       <CraftHeader header="Hold to delete" date="June 25" />
       <div className="flex items-center justify-center w-full h-80 rounded-md relative overflow-hidden bg-[var(--colors-gray1)] border border-[var(--colors-gray6)]">
-        <button className="hold-to-delete-button bg-[var(--colors-gray3)] border border-[var(--colors-gray6)] text-[var(--colors-gray12)] text-lg px-4 py-4 gap-3 select-none touch-manipulation rounded-full active:scale-[0.97]">
-          <div className="hold-overlay absolute inset-0 flex items-center justify-center gap-3 bg-[var(--colors-gray8)] text-[var(--colors-gray1)] rounded-full">
+        <div className="absolute inset-0">
+          <Image
+            src="/img/hold.png"
+            alt="Hold to delete"
+            fill
+            className="object-cover"
+          />
+        </div>
+        <button className="hold-to-delete-button text-[var(--colors-gray12)] text-lg px-4 py-4 gap-3 select-none touch-manipulation rounded-full active:scale-[0.99] bg-black/50 backdrop-blur-[5px] outline-none border-none focus-visible:ring-2 focus-visible:ring-[var(--colors-focus)] focus-visible:ring-offset-1">
+          <div
+            className="hold-overlay absolute inset-0 flex items-center justify-center gap-3 bg-[#fcfcfc] text-[var(--colors-gray1)] rounded-full"
+            style={
+              {
+                "--hold-to-delete-transition-duration": "0.2s",
+              } as React.CSSProperties
+            }
+          >
             <div className="size-6 rounded-full bg-[var(--colors-gray11)] flex items-center justify-center gap-3">
               <div className="size-[11px] rounded-[1px] bg-[var(--colors-gray1)]"></div>
             </div>
