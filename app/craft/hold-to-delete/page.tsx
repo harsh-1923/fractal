ddc2aef4719e6645562497728ec37ca98c0b4ffb
Harsh Sharma <harsh.sharma.001@juspay.in>
2025-06-28 00:57:57 +0530

    light mode fix

--- app/craft/hold-to-delete/page.tsx
+++ app/craft/hold-to-delete/page.tsx
@@ -4,12 +4,12 @@ import React from "react";
 import "./hold-to-delete.css";
 import Image from "next/image";
 
-const page = () => {
+const HoldToDelete = () => {
   return (
     <div className="p-4 pt-20 max-w-3xl mx-auto bg-[var(--background)] min-h-screen">
       <CraftHeader header="Hold to delete" date="June 25" />
       <div className="flex items-center justify-center w-full h-80 rounded-md relative overflow-hidden bg-[var(--colors-gray1)] border border-[var(--colors-gray6)]">
-        <div className="absolute inset-0">
+        <div className="absolute inset-0 select-none">
           <Image
             src="/img/hold.png"
             alt="Hold to delete"
@@ -17,9 +17,9 @@ const page = () => {
             className="object-cover"
           />
         </div>
-        <button className="hold-to-delete-button text-[var(--colors-gray12)] text-lg px-4 py-4 gap-3 select-none touch-manipulation rounded-full active:scale-[0.99] bg-black/50 backdrop-blur-[5px] outline-none border-none focus-visible:ring-2 focus-visible:ring-[var(--colors-focus)] focus-visible:ring-offset-1">
+        <button className="hold-to-delete-button text-[var(--colors-gray12)] text-lg px-4 py-4 gap-3 select-none touch-manipulation rounded-4xl active:scale-[0.99] bg-black/55 backdrop-blur-[4px] outline-none border-none focus-visible:ring-2 focus-visible:ring-[var(--colors-focus)] focus-visible:ring-offset-1">
           <div
-            className="hold-overlay absolute inset-0 flex items-center justify-center gap-3 bg-[#fcfcfc] text-[var(--colors-gray1)] rounded-full"
+            className="hold-overlay absolute inset-0 flex items-center justify-center gap-3 bg-[#fcfcfc] text-[var(--colors-gray1)] rounded-4xl"
             style={
               {
                 "--hold-to-delete-transition-duration": "0.2s",
@@ -34,11 +34,13 @@ const page = () => {
           <div className="size-6 rounded-full bg-[var(--colors-gray1)] flex items-center justify-center gap-3">
             <div className="size-[11px] rounded-[1px] bg-[var(--colors-gray11)]"></div>
           </div>
-          <span className="select-none">Tap and hold to stop</span>
+          <span className="select-none text-[var(--colors-gray1)] dark:text-[var(--colors-gray12)]">
+            Tap and hold to stop
+          </span>
         </button>
       </div>
     </div>
   );
 };
 
-export default page;
+export default HoldToDelete;
