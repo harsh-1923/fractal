20f98fc022558ebcbc8afa934202503024763668
Harsh Sharma <harsh.sharma.001@juspay.in>
2025-06-28 01:07:02 +0530

    fix background image

--- app/craft/hold-to-delete/page.tsx
+++ app/craft/hold-to-delete/page.tsx
@@ -8,16 +8,13 @@ const HoldToDelete = () => {
   return (
     <div className="p-4 pt-20 max-w-3xl mx-auto bg-[var(--background)] min-h-screen">
       <CraftHeader header="Hold to delete" date="June 25" />
-      <div className="flex items-center justify-center w-full h-80 rounded-md relative overflow-hidden bg-[var(--colors-gray1)] border border-[var(--colors-gray6)]">
-        <div className="absolute inset-0 select-none">
-          <Image
-            src="/img/hold.png"
-            alt="Hold to delete"
-            fill
-            className="object-cover"
-          />
-        </div>
-        <button className="hold-to-delete-button text-[var(--colors-gray12)] text-lg px-4 py-4 gap-3 select-none touch-manipulation rounded-4xl active:scale-[0.99] bg-black/55 backdrop-blur-[4px] outline-none border-none focus-visible:ring-2 focus-visible:ring-[var(--colors-focus)] focus-visible:ring-offset-1">
+      <div
+        className="flex items-center justify-center w-full h-80 rounded-md relative overflow-hidden bg-[var(--colors-gray1)] border border-[var(--colors-gray6)] bg-cover"
+        style={{
+          backgroundImage: "url(/img/hold.png)",
+        }}
+      >
+        <button className="hold-to-delete-button text-[var(--colors-gray12)] text-lg px-4 py-4 gap-3 select-none touch-manipulation rounded-4xl active:scale-[0.99] bg-black/55 backdrop-blur-[4px] outline-none border-none focus-visible:ring-2 focus-visible:ring-[var(--colors-focus)] focus-visible:ring-offset-1 shadow-2xl">
           <div
             className="hold-overlay absolute inset-0 flex items-center justify-center gap-3 bg-[#fcfcfc] text-[var(--colors-gray1)] rounded-4xl"
             style={
